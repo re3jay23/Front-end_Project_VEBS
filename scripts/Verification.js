10178cc0b46ccce014802f64269e026d3a7a5a35
re3jay23 <ari_widjaja@hotmail.com>
2018-03-24 03:29:06 -0700

    add child form and functions to manipulate/ animate child form

--- scripts/Verification.js
+++ scripts/Verification.js
@@ -1,7 +1,5 @@
 
-$(document).ready(function(){
-	
-})
+
 
 var temp_number =[];
 const numerical_position=[1,2,3,5,6,7,9,10,11,12];
@@ -22,15 +20,7 @@ function verifyZip(input){
 function verifyPhone(i){
 
 	var notification = document.getElementById("Telephone_notification");
-	//console.log("hello I am here", i.value);
-	// var type1 = (/\(\d{3}\)\d{3}-\d{4}/).test(i.value);
-	// var type2 = (/\d{10}/).test(i.value);
-	//console.log("type1: ", type1);
-	//console.log("type2: ", type2);
 	var telephoneFormat =(/\(?\d{3}[\)|-](\d{3})-(\d{4})/).test(i.value);
-
-	//console.log("telephoneFormat: ", telephoneFormat);
-	//console.log("length input", i.value.length);
 	try{
 		if(telephoneFormat == false || i.value.length >13) throw "Please type 10 digit phone number in the following format (123)456-7891";
 		//else if(type2) throw "Please type as in this format (XXX)XXX-XXXX";
@@ -58,6 +48,7 @@ function verifyDOB(i,DOB_span){
 	}
 }
 
+//** to display/ hide Spouse form
 function displaySpouseForm(a){
 	if(a){
 		document.getElementById('SpouseForm_col').style.display = "block";
@@ -67,10 +58,18 @@ function displaySpouseForm(a){
 
 }
 
+//** to display/ hide Child form
+function displayChildForm(a){
+	if(a){
+		$('#ChildForm_col').css("display","block");
+	}else{
+		$('#ChildForm_col').css("display","none");
+	}
+
+}
+
 function verifyAge(i,Age_span){
 	var ageFormat =(/\d{2}/).test(i.value);
-	//console.log("i value", i.value);
-	//console.log("ageformat: ", ageFormat)
 	var age_errorspan = document.getElementById(Age_span);
 	try{
 		if(ageFormat == false || i.value.length >2) throw "Please revise and enter your age";
@@ -82,3 +81,81 @@ function verifyAge(i,Age_span){
 
 	}
 }
+
+//** initialize State dropdown list
+function initializeState(){
+	const states=["---","Alabama","Alaska","Arizona","Arkansas",
+								"California","Colorado","Connecticut","Delaware","District of Columbia",
+								"Florida","Georgia","Guam","Hawaii","Idaho","Illinois","Indiana",
+								"Iowa","Kansas","Kentucky","Louisiana","Maine","Maryland","Massachusetts",
+								"Michigan","Minnesota","Mississippi","Missouri","Montana","Nebraska","Nevada",
+								"New Hampshire","New Jersey","New Mexico","New York","North Carolina","North Dakota",
+								"Northern Marianas Islands","Ohio","Oklahoma","Oregon","Pennsylvania","Puerto Rico",
+								"Rhode Island","South Carolina","South Dakota","Tennessee","Texas","Utah","Vermont",
+								"Virginia","Virgin Islands","Washington","West Virginia","Wisconsin","Wyoming"];
+	var textholder;
+	for(var i = 0; i < states.length; i++){
+			textholder += "<option value='"+states[i]+"'>"+states[i]+"</option>"
+	}
+	$('#State').append(textholder);
+}
+
+
+//** Functions to initialize dropdown lists for D.O.B
+function initializeDayMonthYear(){
+	var appendDay, appendMonth, appendYear;
+	for(var i = 1; i<=31;i++){
+		 appendDay +="<option value='"+i+"'>"+i+"</option>";
+
+	}
+	var months =["January","February","March","April",
+								"May","June","July","August","September",
+								"October","November","December"];
+	for(var x = 0; x<months.length;x++){
+			appendMonth +="<option value='"+months[x]+"'>"+months[x]+"</option>";
+
+	}
+	for(var y = 2018; y>=1950;y--){
+		appendYear +="<option value='"+y+"'>"+y+"</option>";
+
+	}
+	$('#DOBDay_select, #DOBSpouseDay_select').append(appendDay);
+	$('#DOBMonth_select, #DOBSpouseMonth_select').append(appendMonth);
+	$('#DOBYear_select, #DOBSpouseYear_select').append(appendYear);
+}
+
+
+
+//** aggregate and append Child ages form dynamically
+var childCounter =2;
+function appendChildAgeList(){
+//console.log("in appendChildAgeList function");
+
+	var textholder ="<tr id='child_tr-"+childCounter+"'><td> Child "+childCounter+"</td><td><input type='number' min='1' max='17' class='Childtd-input'></input> years old</td></tr>"
+	childCounter +=1;
+	$('#ChildAge_tbl').append(textholder);
+}
+
+function removeChildAgeList(){
+	try{
+		if(childCounter == 2) throw "Cannot remove."
+		else{
+			var lastID = "#child_tr-"+(childCounter-1);
+
+			$(lastID).remove();
+			childCounter -=1;
+
+		}
+	}catch(err){
+		$('#Childcount_span').fadeIn(3000,function(){
+			$('#Childcount_span').text(err);
+			$('#Childcount_span').fadeOut(2000);
+		});
+	}
+
+}
+
+$(document).ready(function(){
+	initializeDayMonthYear();
+	initializeState();
+})
